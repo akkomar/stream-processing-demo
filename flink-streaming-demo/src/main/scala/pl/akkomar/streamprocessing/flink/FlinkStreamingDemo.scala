f96dbd93dd0fe858ed4367d2703965d54aa4bbe1
Arkadiusz Komarzewski <ak.komar@gmail.com>
2017-01-26 20:09:42 +0100

    wip

--- flink-streaming-demo/src/main/scala/pl/akkomar/streamprocessing/flink/FlinkStreamingDemo.scala
+++ flink-streaming-demo/src/main/scala/pl/akkomar/streamprocessing/flink/FlinkStreamingDemo.scala
@@ -4,6 +4,7 @@ import java.util.Properties
 
 import org.apache.flink.api.scala._
 import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
+import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer09
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema
 import pl.akkomar.streamprocessing.CommonConfig
@@ -21,6 +22,8 @@ object FlinkStreamingDemo {
       CommonConfig.TweetsTopic,
       new SimpleStringSchema(),
       properties))
+
+    messageStream.map(_.split(" ")).filter(_.startsWith("#")).timeWindowAll(Time.minutes(1),Time.seconds(15)).
 
     messageStream.print()
 
